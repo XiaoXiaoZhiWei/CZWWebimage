f3b8a883fb7e3962e191d63893c517c590e7e62d
崔志伟 <2213139162@qq.com>
2019-04-09 15:53:57 +0800

    1.初始化项目

---
+++ CZWWebimage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CZWWebimage
+//
+//  Created by dingdang on 2019/4/9.
+//  Copyright © 2019 dingdang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CZWWebimage/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CZWWebimage
+//
+//  Created by dingdang on 2019/4/9.
+//  Copyright © 2019 dingdang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
